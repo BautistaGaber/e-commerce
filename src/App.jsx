eb09b51a2a0a5b325879fbd2e29c64411eb69f50
BautistaGaber <bautistagaber@gmail.com>
2023-10-01 13:40:37 -0300

    Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Products from './components/Products'
 import NavBar from './components/NavBar'
+import Home from './components/Home'
+import NotFound from './components/NotFound/NotFound'
 
 function App() {
   return (
     <>
-      <NavBar/> 
-      <Products/>
+      <BrowserRouter>
+      <NavBar/>   
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='products' element={<Products/>}/>
+        <Route path='products/*' element={<NotFound/>}/>
+        <Route path='/*' element={<NotFound/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
