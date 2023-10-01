ab3e386c10702a9da7dbf21b8141706d615dc2ae
BautistaGaber <bautistagaber@gmail.com>
2023-10-01 13:41:57 -0300

    Add new styles

---
+++ src/components/Products/ButtonDetails.jsx
@@ -0,0 +1,11 @@
+const ButtonDetails = ({buttonName}) => {
+    return(
+        <div className="button-details">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                {buttonName}
+            </button>
+        </div>
+    ) 
+}
+
+export default ButtonDetails
--- src/components/Products/ProductsContainer.jsx
+++ src/components/Products/ProductsContainer.jsx
@@ -1,7 +1,9 @@
+import ButtonDetails from './ButtonDetails'
+
 const ProductsContainer = ({ product }) => {
   return (
     <>
-      <div className="hover:opacity-75 ">
+      <div className="hover:opacity-75">
         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-2 00 lg:aspect-none group-hover:opacity-75 lg:h-80">
           <img src={product.image} alt="Front of men&#039;s Basic Tee in black." className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
         </div>
@@ -19,6 +21,7 @@ const ProductsContainer = ({ product }) => {
             </div>
             <p className="text-sm font-medium text-gray-900">{product.price}</p>
           </div>
+          <ButtonDetails buttonName = 'detalle' />
         </div>
       </div>
     </>
