628acb1c385d43adc535a6dec580ef82a8dffe9e
BautistaGaber <bautistagaber@gmail.com>
2023-10-21 20:33:42 -0300

    add cartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Products from './components/Products'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
 import NotFound from './components/NotFound/NotFound'
 import ProductsById from './components/Products/ProductsById'
 import Footer from './components/Footer'
+import { CartProvider } from './context/CartContext'
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <NavBar/>   
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='products' element={<Products/>}/>
-        <Route exact path='products/:id' element={<ProductsById/>}/>
-        <Route path='*' element={<NotFound/>}/>
-      </Routes>
-      <Footer/>
+        <CartProvider>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='products' element={<Products />} />
+            <Route exact path='products/:id' element={<ProductsById />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </CartProvider>
       </BrowserRouter>
     </>
   )
