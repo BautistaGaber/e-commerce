06d144219a4fd25e243678905c2e1ae8282ea9ba
BautistaGaber <bautistagaber@gmail.com>
2023-10-22 00:28:34 -0300

    style changes

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -52,12 +52,6 @@ const NavBar = () => {
                             </div>
 
                             <div className="ml-auto flex items-center">
-                                {/* <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                                        <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">Sign in</a>
-                                        <span className="h-6 w-px bg-gray-200" aria-hidden="true"></span>
-                                        <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">Create account</a>
-                                    </div> */}
-
                                 <div className="hidden lg:ml-8 lg:flex">
                                     <a href="#" className="flex items-center text-gray-700 hover:text-gray-800">
                                         <img src="https://tailwindui.com/img/flags/flag-canada.svg" alt="" className="block h-auto w-5 flex-shrink-0" />
@@ -79,7 +73,7 @@ const NavBar = () => {
                                 {/* Cart */}
                                 <div className="ml-4 flow-root lg:ml-6">
                                     <a href="#" className="group -m-2 flex items-center p-2">
-                                        <CartWidget />
+                                    <NavLink className="flex" to="/products/cart"><CartWidget/></NavLink>
                                     </a>
                                 </div>
                             </div>
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,7 +5,6 @@ export const CartContext = createContext({})
 export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([])
 
-
     return (
         <CartContext.Provider value={[cart, setCart]}>
             {children}
