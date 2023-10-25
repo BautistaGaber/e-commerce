bbb46baa23ffa58c3c8d13b857afa4a72652a787
BautistaGaber <bautistagaber@gmail.com>
2023-10-25 20:56:01 -0300

    style changes

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -24,11 +24,11 @@ const ItemListContainer = () => {
     return (
       <div className="bg-white">
         <div className=" mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="animate-pulse text-2xl font-bold tracking-tight text-gray-900"></h2>
-          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {Array.from(new Array(12)).map(() => {
+          <div className="animate-pulse border bg-gray-400 shadow rounded-md p-4 max-w-sm w-full mx-auto h-16"></div>
+          <div className="mt-8 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {Array.from(new Array(20)).map(() => {
               return (
-                <div key={crypto.randomUUID()} className="animate-pulse border bg-gray-400 shadow rounded-md p-4 max-w-sm w-full mx-auto h-80"></div>                                               
+                <div key={crypto.randomUUID()} className="animate-pulse border bg-gray-400 shadow rounded-md p-4 max-w-sm w-full mx-auto h-96"></div>                                               
               )
             })}
           </div>
@@ -47,7 +47,7 @@ const ItemListContainer = () => {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">All products</h2>
+        <h2 className="flex justify-center text-5xl font-bold tracking-tight text-gray-900">All products</h2>
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {
             products.map((product) => (
